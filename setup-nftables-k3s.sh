b54c12a70183c58b3cd8bc3cea8b40413c55c451
brynjolm <130877488+brynjolm@users.noreply.github.com>
2023-08-08 04:48:37 +0200

    Create setup-nftables-k3s.sh

---
+++ setup-nftables-k3s.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Step 1: Create nftables configuration file
+cat <<EOF >/etc/nftables-k3s.conf
+#!/usr/sbin/nft -f
+
+table inet filter {
+    chain input {
+        type filter hook input priority 0; policy accept;
+
+        ct state established,related accept
+        iifname "lo" accept
+        tcp dport 22 accept
+        tcp dport 6443 accept
+        accept
+    }
+
+    chain forward {
+        type filter hook forward priority 0; policy drop;
+    }
+
+    chain output {
+        type filter hook output priority 0; policy accept;
+    }
+}
+EOF
+
+# Step 2: Load nftables rules
+nft -f /etc/nftables-k3s.conf
+
+# Step 3: Save nftables rules for persistence
+nft list ruleset > /etc/nftables-k3s.rules
+
+# Step 4: Create and enable systemd service
+cat <<EOF >/etc/systemd/system/nftables-k3s.service
+[Unit]
+Description=nftables rules for k3s
+After=nftables.service
+
+[Service]
+ExecStart=/usr/sbin/nft -f /etc/nftables-k3s.rules
+Type=oneshot
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable nftables-k3s.service
+
+# Step 5: Reboot the system
+reboot
