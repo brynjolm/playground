6b63c78c6a62844df91456c4114e70e2086e95d2
brynjolm <130877488+brynjolm@users.noreply.github.com>
2023-08-08 21:50:11 +0200

    Update setup_kubectl.sh

--- setup_kubectl.sh
+++ setup_kubectl.sh
@@ -36,7 +36,7 @@ mkdir -p ~/.kube
 export KUBECONFIG=~/.kube/config
 
 # Copy k3s.yaml from remote machine
-scp nick@192.168.200.100:/etc/rancher/k3s/k3s.yaml ~/.kube/config
+scp nick@192.168.200.10:/etc/rancher/k3s/k3s.yaml ~/.kube/config
 
 # Update k3s master node IP in k3s.yaml
 sed -i "s/server:.*/server: https:\/\/192.168.200.100:6443/g" ~/.kube/config
